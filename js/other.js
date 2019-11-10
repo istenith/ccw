735d6dc99b61f61002aef273e1054978ba07a022
Yash Punia <puniayash@gmail.com>
2019-11-10 17:05:42 +0530

    added other stories

---
+++ js/other.js
@@ -0,0 +1,17 @@
+var cards = document.getElementsByClassName('story-card');
+
+for(let i=0;i<cards.length;i++) {
+    cards[i].style.height = '100px';
+}
+
+function showMore(e)
+{
+    let currentHeight = e.target.style.height;
+    if(currentHeight == '100px') {
+        e.target.style.height = 'fit-content';
+    }
+    else {
+        e.target.style.height = '100px';
+    }
+    
+}
